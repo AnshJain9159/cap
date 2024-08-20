0074d44f4767b95c0458a4b11805c0e1dc4a849c
Ansh Jain <anshjain9159@gmail.com>
2024-08-20 19:41:44 +0530

    bugs : bugs fix

--- client/src/components/AdminPage.tsx
+++ client/src/components/AdminPage.tsx
@@ -1,12 +1,11 @@
-import React from "react";
-import AdminPanel from "./Admin";
+// import AdminPanel from "./Admin";
 
-function AdminPage() {
-  return (
-    <div>
-      <AdminPanel />
-    </div>
-  );
-}
+// function AdminPage() {
+//   return (
+//     <div>
+//       <AdminPanel />
+//     </div>
+//   );
+// }
 
-export default AdminPage;
+// export default AdminPage;
